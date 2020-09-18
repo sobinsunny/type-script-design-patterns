7911d7982ae28412cd2d8ce06642e35f611485c8
suresh varman k <varman@sureshs-MacBook-Pro.local>
2020-09-18 21:03:29 +0530

    updated

--- structural/proxy.ts
+++ structural/proxy.ts
@@ -1,3 +1,8 @@
+/**
+ * Proxy pattern ensures accessibility by introducing an proxy class between client and the real class
+ * 
+ * e.g. To have an coffee, you can't reach to the coffeeMaker, without your wife's daily limit checl.
+ */
 interface CoffeeI {
     getMeACoffee(): void
 }
